436fd8c427a9104ff2986faf5387338e9e099b87
cristimaxinmihai <cristimaxinmihai@yahoo.com>
2026-09-06 00:08:46 -0500

    Add CORS headers to checkout endpoint
    Add CORS headers to checkout session API.

--- api/create-checkout-session.js
+++ api/create-checkout-session.js
@@ -14,6 +14,13 @@ const PRICE_IDS = {
 };
 
 module.exports = async (req, res) => {
+res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
   if (req.method !== 'POST') {
     res.status(405).json({ error: 'Method not allowed' });
     return;
